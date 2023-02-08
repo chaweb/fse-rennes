8ecb2b5eccdfde9966562b129255c1a2e6b1e2af
chaweb <46398015+chaweb@users.noreply.github.com>
2023-02-08 23:22:16 +0100

    Update ecosystem.config.js

--- .server/ecosystem.config.js
+++ .server/ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {  
-        apps: [{
-        name: 'test.chaweb.fr',
-        port: '1080',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: '/index.mjs',
-    }]
+    apps: [{
+    name: 'test.chaweb.fr',
+    port: '1080',
+    exec_mode: 'cluster',
+    instances: 'max',
+    script: './server/index.mjs',
+}]
 }
